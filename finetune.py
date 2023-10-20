227fb0f2f0621f40900404f24960fe4aad3d142d
theabrusch <62242837+theabrusch@users.noreply.github.com>
2023-10-20 10:50:41 +0200

    update to load pretrained model

--- finetune.py
+++ finetune.py
@@ -2,6 +2,7 @@ import torch
 import argparse
 from src.multiview import load_model, finetune, evaluate_classifier
 from src.eegdataset import construct_eeg_datasets
+from src.dataset import get_datasets
 from torch.optim import AdamW
 import numpy as np
 from sklearn.utils.class_weight import compute_class_weight
@@ -27,43 +28,50 @@ def main(args):
     args.standardize_epochs = 'channelwise'
     
     # load data 
-    _, _, finetune_loader, finetune_val_loader, test_loader, (channels, time_length, num_classes) = construct_eeg_datasets(**vars(args))
+    finetune_loader, finetune_val_loader, test_loader, (channels, time_length, num_classes) = get_datasets(args.data_path, args.batchsize)
     orig_channels = channels
+    finetune_loader = [finetune_loader]
+    finetune_val_loader = [finetune_val_loader]
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dset = args.data_path.split('/')[-2]
 
     if args.load_model:
-        pretrained_model_path = f'pretrained_models/MultiView_{args.pretraining_setup}_{args.loss}'
-        output_path = f'finetuned_models/MultiView_{args.pretraining_setup}_{args.loss}'
+        pretrained_model_path = f'pretrained_models/{args.pretraining_dset}_{args.pretraining_setup}_{args.loss}'
+        output_path = f'pretrained_models/{dset}_{args.pretraining_setup}_{args.loss}'
         group = f'{args.pretraining_setup}_{args.loss}'
         model_arg_path = pretrained_model_path + '/args.pkl'
         with open(model_arg_path, 'rb') as f:
             model_args = pickle.load(f) 
     else:
-        output_path = f'finetuned_models/MultiView_{args.pretraining_setup}_scratch'
+        output_path = f'finetuned_models/{args.pretraining_setup}_scratch'
         group = f'{args.pretraining_setup}_scratch'
         model_args = args
+        model_args.channels = channels
+        model_args.time_length = time_length
+        model_args.num_classes = num_classes
 
     output_path = check_output_path(output_path)
     args.outputh_path = output_path
     print('Saving outputs in', output_path)
 
     for ft_loader, ft_val_loader in zip(finetune_loader, finetune_val_loader):
-        wandb.init(project = 'MultiView', group = group, config = args)
-        model, loss_fn = load_model(args.pretraining_setup, device, channels, time_length, num_classes, model_args)
-        train_samples = len(ft_loader.sampler)
-        val_samples = len(ft_val_loader.sampler)
+        wandb.init(project = 'MultiView_new', group = group, config = args)
+        model, loss_fn = load_model(args.pretraining_setup, device, model_args)
+        train_samples = len(ft_loader.dataset)
+        val_samples = len(ft_val_loader.dataset)
 
         if args.load_model:
             pretrained_model_path = pretrained_model_path + '/pretrained_model.pt'
             model.load_state_dict(torch.load(pretrained_model_path, map_location=device))
+            model.remove_projector()
+            model.update_classifier(num_classes, orig_channels=orig_channels, pool = args.pool, seed = args.seed)
         
         ft_output_path = output_path + f'/{train_samples}_samples'
         os.makedirs(ft_output_path, exist_ok=True)
 
         wandb.config.update({'Finetune samples': train_samples, 'Finetune validation samples': val_samples, 'Test samples': len(test_loader.dataset)})
 
-        model.update_classifier(num_classes, orig_channels=orig_channels, seed = args.seed)
         model.to(device)
 
         if args.optimize_encoder:
@@ -71,19 +79,19 @@ def main(args):
         else:
             optimizer = AdamW(model.classifier.parameters(), lr = args.ft_learning_rate, weight_decay=args.weight_decay)
         
-        if not args.balanced_sampling == 'finetune' or args.balanced_sampling == 'both':
-            targets = ft_loader.dataset.dn3_dset.get_targets()
-            weights = torch.tensor(compute_class_weight('balanced', classes = np.unique(targets), y = targets)).float().to(device)
-            wandb.config.update({'Target distribution': np.unique(targets, return_counts=True)[-1]})
-        else:
-            weights = None
+        #if not args.balanced_sampling == 'finetune' or args.balanced_sampling == 'both':
+        #    targets = ft_loader.dataset.dn3_dset.get_targets()
+        #    weights = torch.tensor(compute_class_weight('balanced', classes = np.unique(targets), y = targets)).float().to(device)
+        #    wandb.config.update({'Target distribution': np.unique(targets, return_counts=True)[-1]})
+        #else:
+        #    weights = None
         
         finetune(model,
                 ft_loader,
                 ft_val_loader,
                 args.finetune_epochs,
                 optimizer,
-                weights,
+                None,
                 device,
                 test_loader = test_loader if args.track_test_performance else None,
                 early_stopping_criterion=args.early_stopping_criterion,
@@ -102,24 +110,27 @@ if __name__ == '__main__':
     parser.add_argument('--job_id', type = str, default = '0')
     # whether or not to save finetuned models
     parser.add_argument('--save_model', type = eval, default = False)
-    parser.add_argument('--load_model', type = eval, default = False)
+    parser.add_argument('--load_model', type = eval, default = True)
     parser.add_argument('--optimize_encoder', type = eval, default = False)
-    parser.add_argument('--pretraining_setup', type = str, default = 'MPNN', options = ['MPNN', 'nonMPNN'])
+    parser.add_argument('--pretraining_dset', type = str, default = 'HAR')
+    parser.add_argument('--pretraining_setup', type = str, default = 'MPNN', choices = ['MPNN', 'nonMPNN'])
+
     parser.add_argument('--seed', type = int, default = 42)
 
     # data arguments
     # path to config files. Remember to change paths in config files. 
-    parser.add_argument('--data_path', type = str, default = 'sleepps18.yml')
-    parser.add_argument('--finetune_path', type = str, default = 'sleepedf.yml')
+    parser.add_argument('--data_path', type = str, default = '/Users/theb/Desktop/data/Gesture/')
+    #parser.add_argument('--finetune_path', type = str, default = 'sleepedf.yml')
     # whether or not to sample balanced during finetuning
     parser.add_argument('--balanced_sampling', type = str, default = 'finetune')
     # number of samples to finetune on. Can be list for multiple runs
     parser.add_argument('--sample_generator', type = eval, nargs = '+', default = [10, 20, None])
 
     # model arguments
-    parser.add_argument('--layers', type = int, default = 6)
+    parser.add_argument('--nlayers', type = int, default = 6)
     # early stopping criterion during finetuning. Can be loss or accuracy (on validation set)
-    parser.add_argument('--early_stopping_criterion', type = str, default = None, options = [None, 'loss', 'acc'])
+    parser.add_argument('--early_stopping_criterion', type = str, default = None, choices = [None, 'loss', 'acc'])
+    parser.add_argument('--pool', type = str, default = 'adapt_avg', choices = ['adapt_avg', 'flatten'])
     parser.add_argument('--conv_do', type = float, default = 0.1)
     parser.add_argument('--feat_do', type = float, default = 0.1)
     parser.add_argument('--num_message_passing_rounds', type = int, default = 3)
@@ -135,13 +146,13 @@ if __name__ == '__main__':
     parser.add_argument('--sample_test_subjects', type = eval, default = False)
 
     # optimizer arguments
-    parser.add_argument('--loss', type = str, default = 'time_loss', options = ['time_loss', 'contrastive', 'COCOA'])
+    parser.add_argument('--loss', type = str, default = 'contrastive', choices = ['time_loss', 'contrastive', 'COCOA'])
     # whether or not to compute performance on test set during training
     parser.add_argument('--track_test_performance', type = eval, default = True)
     parser.add_argument('--ft_learning_rate', type = float, default = 1e-3)
     parser.add_argument('--weight_decay', type = float, default = 5e-4)
     parser.add_argument('--finetune_epochs', type = int, default = 1)
-    parser.add_argument('--target_batchsize', type = int, default = 128)
+    parser.add_argument('--batchsize', type = int, default = 128)
     args = parser.parse_args()
     main(args)
 
