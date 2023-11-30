35a4962ea163087a6085ab868d4b02fe0292eca1
theabrusch <62242837+theabrusch@users.noreply.github.com>
2023-11-30 10:48:09 +0100

    squeeze training target

--- src/datasets/datasets.py
+++ src/datasets/datasets.py
@@ -179,9 +179,9 @@ def get_simulated_data_finetuning(finetune_setup,
     val = simulator.generate(samples[1], random_freqs=True)
     test = simulator.generate(samples[2], random_freqs=True)
 
-    X_train, y_train = torch.Tensor(train[0]).transpose(1,2), torch.Tensor(train[1]).long().squeeze(1)
-    X_val, y_val = torch.Tensor(val[0]).transpose(1,2), torch.Tensor(val[1]).long().squeeze(1)
-    X_test, y_test = torch.Tensor(test[0]).transpose(1,2), torch.Tensor(test[1]).long().squeeze(1)
+    X_train, y_train = torch.Tensor(train[0]).transpose(1,2), torch.Tensor(train[1]).long().squeeze()
+    X_val, y_val = torch.Tensor(val[0]).transpose(1,2), torch.Tensor(val[1]).long().squeeze()
+    X_test, y_test = torch.Tensor(test[0]).transpose(1,2), torch.Tensor(test[1]).long().squeeze()
 
     train_dset = SSL_dataset(X_train, y_train, standardize_channels=standardize_channels)
     val_dset = SSL_dataset(X_val, y_val, standardize_channels=standardize_channels)
